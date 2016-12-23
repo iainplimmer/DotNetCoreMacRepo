698b6ed4d4c3c388763773a0e5ee35528cff56ff
iain plimmer <iainplimmer@iains-iMac.local>
2016-12-23 13:21:05 +0000

    Removal of un-needed dependencies.

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,3 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -9,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using IainPlimmerApi.Interfaces;
 using IainPlimmerApi.Repositories;
-using Microsoft.AspNetCore;
 
 namespace IainPlimmerApi
 {
